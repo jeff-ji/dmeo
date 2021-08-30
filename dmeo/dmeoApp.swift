37bb13afc72befe53dff8f29d84bee2a78247a98
jeff-ji <68872443@qq.com>
2021-08-30 19:06:36 +0800

    Initial Commit

---
+++ dmeo/dmeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dmeoApp.swift
+//  dmeo
+//
+//  Created by jeff ji on 2021/8/21.
+//
+
+import SwiftUI
+
+@main
+struct dmeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
